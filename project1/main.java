5b07f137649347e544eb2bd65fd1257017e66033
Bender <zron25@gmail.com>
2026-02-06 13:10:37 -0500

    updated first project

---
+++ project1/main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class main {
+  public static void main(String[] args)
+  {
+    Printarraylist;
+  }
+  public static void Printarraylist
+  {
+    List<String> listStrings = new ArrayList<String>();
+    listStrings.add("One");
+    listStrings.add("Two");
+    listStrings.add("Three");
+    listStrings.add("Four");
+    System.out.println(listStrings);
+  }
+}
